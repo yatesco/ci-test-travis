b14a72c007eddea44082254a4cdcb16475ddfb3f
Colin Yates <colin.yates@qficonsulting.com>
2015-10-05 10:51:03 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ci-test-travis
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ci_test_travis/core_test.clj
@@ -0,0 +1,7 @@
+(ns ci-test-travis.core-test
+  (:require [clojure.test :refer :all]
+            [ci-test-travis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
